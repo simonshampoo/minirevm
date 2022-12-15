7517d0445d16a28290fefe0ac1db3565ad965624
Simon Shamoon <simonshm5@gmail.com>
2022-12-14 21:48:55 -0600

    planning phase

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 fn main() {
 
-    aiosdhaosiudhasuiodh
+    /*
 
+
+        this program will take in bytecode
+
+        then it will simulate the memory, storage, calldata, and stack
+
+        similar to evm.codes playground
+
+        it will be a light version of the evm binary in geth
+
+
+
+    */
 }
--- src/storage.rs
+++ src/storage.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, str::Bytes};
 
 #[derive(PartialOrd, PartialEq, Eq)]
 struct Bytes32 {
@@ -6,7 +6,20 @@ struct Bytes32 {
 }
 
 impl core::cmp::Ord for Bytes32 {
-    fn cmp(&self, other: &Bytes32) -> std::cmp::Ordering {}
+    fn cmp(&self, other: &Bytes32) -> std::cmp::Ordering {
+        for i in 0..3 {
+            if other.store[i] > self.store[i] {
+                return std::cmp::Ordering::Greater;
+            }
+            if other.store[i] < self.store[i] {
+                return std::cmp::Ordering::Less;
+            }
+        }
+
+        std::cmp::Ordering::Equal
+    }
 }
 
-pub struct Storage {}
+pub struct Storage {
+    kvstore: HashMap<Bytes32, Bytes32>,
+}
