86e1246931be7bf3cccbca8d40ebcde3eabf1086
Simon Shamoon <simonshm5@gmail.com>
2022-12-15 23:32:41 -0600

    SO HUNGIESS

--- src/stack.rs
+++ src/stack.rs
@@ -1,9 +1,13 @@
 struct Stack {
-    stack: Vec<u8>,
+    stack: Vec<Bytes32>,
 }
 
 impl Stack {
     fn new(&self) -> Self {
-        Stack { stack: vec![] }
+        Stack { stack: Vec<u8>::new() }
+    }
+
+    fn push(&mut self, value: Bytes32) -> Self {
+        self.stack.push(value)
     }
 }
