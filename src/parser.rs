68ba57dad59c84a09f03c6f146745b9677256211
Simon Shamoon <simonshm5@gmail.com>
2022-12-21 15:31:07 -0600

    push data size

--- src/parser.rs
+++ src/parser.rs
@@ -24,6 +24,21 @@ impl Parser {
         while i < bytecode.len() - 2 {
             let opcode = str::from_utf8(&bytecode.as_bytes()[i..i + 2]);
             i += 2;
+
+            match opcode.unwrap() {
+                "60" => println!("push"),
+                &_ => panic!("AAAA"),
+            }
+
+            /*
+
+            match opcode {
+                <opcode_that_takes_stack_input> => look at stack and and continue otherwise
+                <opcode_that_doesnt_take_stack_input> =>
+            }
+
+
+            */
             self.instructions.push((
                 Opcode {
                     0: opcode.unwrap().as_ptr() as u8,
