ed6972719851fe7e5fd60edbd7d66c0377ea2fee
Simon Shamoon <simonshm5@gmail.com>
2022-12-22 22:48:46 -0600

    push me off a bridge

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,5 @@
-use crate::{types::Instruction, utils::match_push_n};
+use crate::types::Instruction;
 use evm::Opcode;
-use hex::encode;
-use std::str;
 pub struct Parser {
     instructions: Vec<Instruction>,
 }
@@ -15,29 +13,19 @@ impl Parser {
     pub fn parse(&mut self, bytecode: &String) {
         let codesize = bytecode.len();
 
-        if codesize == 0 {
-            panic!("no bytecode provided")
-        }
+        if codesize == 0 {}
 
         for i in 0..=codesize - 2 {
             let opcode = Opcode(bytecode[i..i + 2].parse().unwrap());
 
-            let jumps = 0;
-            if let None = opcode.is_push() { // if we don't have a PUSH opcode, meaning we don't take an immediate value but possibly from the stack
+            let mut pushdata_len: usize = 0;
+            if let Some(len) = opcode.is_push() {
+                pushdata_len = len as usize;
             }
 
-            //
-            //            let jumps = match opcode {
-            //                Opcode(0x60).as_u..=Opcode(0x7f)=> match_push_n(opcode),
-            //                _ => usize::MAX,
-            //            };
-
-            if jumps == usize::MAX {
-                panic!("what the fuck")
-            }
-
+            if pushdata_len != 0 {}
             self.instructions
-                .push((Opcode { 0: opcode.0 }, Some(vec![String::from("IDK")])));
+                .push((Opcode { 0: opcode.0 }, Some(String::from("IDK"))));
 
             println!("OPCODE: {}", opcode.0);
             /*
--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,4 @@
 use evm::Opcode;
 
 type PushData = String;
-pub type Instruction = (Opcode, Option<Vec<PushData>>);
+pub type Instruction = (Opcode, Option<PushData>);
