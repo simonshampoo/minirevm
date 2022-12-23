c6447401815234e1b62c8a3d244be973ab502107
Simon Shamoon <simonshm5@gmail.com>
2022-12-23 00:15:44 -0600

    _

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,6 @@
 use crate::types::Instruction;
 use evm::Opcode;
+use std::i64;
 pub struct Parser {
     instructions: Vec<Instruction>,
 }
@@ -10,19 +11,30 @@ impl Parser {
             instructions: Vec::new(),
         }
     }
-    pub fn parse(&mut self, bytecode: &String) {
+    pub fn parse(&mut self, bytecode: &String) -> &Vec<Instruction> {
         let codesize = bytecode.len();
 
-        if codesize == 0 {}
+        if codesize == 0 {
+            panic!("no bytecode provided");
+        }
         let mut i = 0;
         while i < codesize - 2 {
-            let opcode = Opcode(bytecode[i..i + 2].parse().unwrap());
+            // how tf to parse hexadecimal strings as hexadecimal numbers
+            println!(
+                "bytecode parse test: {:?}",
+                i64::from_str_radix(&bytecode[i..i + 2], 16)
+            );
+            let opcode = Opcode {
+                0: i64::from_str_radix(&bytecode[i..i + 2], 16).unwrap() as u8,
+            };
 
+            println!("OPCODE {}", &opcode.0);
             let mut pushdata_len: usize = 0;
             if let Some(len) = opcode.is_push() {
-                pushdata_len = len as usize;
+                pushdata_len = (len * 2) as usize;
+                println!("do we ever go here? {}", pushdata_len);
+                // NEWSFLASH BUDDY: WE DONT AND ITS BECAUSE HEX STRING AND u8 AND SHIT IS CONFUSING ME
             }
-
             if pushdata_len != 0 {
                 let immediate_val = &bytecode[i + 2..pushdata_len];
                 self.instructions
@@ -30,10 +42,11 @@ impl Parser {
                 i += pushdata_len;
             } else {
                 i += 2;
+                self.instructions.push((Opcode { 0: opcode.0 }, None));
             }
-            println!("OPCODE: {}", opcode.0);
         }
-
         println!("Instructions received: {:?}", self.instructions);
+
+        &self.instructions
     }
 }
