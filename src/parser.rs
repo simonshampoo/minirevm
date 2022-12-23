60adaef03425f033eb8f29aec9b86f71718b8be7
Simon Shamoon <simonshm5@gmail.com>
2022-12-23 17:19:07 -0600

    parsed push opcodes successfully ayayayay

--- src/parser.rs
+++ src/parser.rs
@@ -18,34 +18,32 @@ impl Parser {
             panic!("no bytecode provided");
         }
         let mut i = 0;
-        while i < codesize - 2 {
+        while i < codesize {
             // how tf to parse hexadecimal strings as hexadecimal numbers
-            println!(
-                "bytecode parse test: {:?}",
-                i64::from_str_radix(&bytecode[i..i + 2], 16)
-            );
             let opcode = Opcode {
                 0: i64::from_str_radix(&bytecode[i..i + 2], 16).unwrap() as u8,
             };
 
-            println!("OPCODE {}", &opcode.0);
+            println!("OPCODE 0x{:x?}", &opcode.0);
+
+            i += 2;
             let mut pushdata_len: usize = 0;
             if let Some(len) = opcode.is_push() {
                 pushdata_len = (len * 2) as usize;
-                println!("do we ever go here? {}", pushdata_len);
+                println!("pushdata_length: {}", pushdata_len);
                 // NEWSFLASH BUDDY: WE DONT AND ITS BECAUSE HEX STRING AND u8 AND SHIT IS CONFUSING ME
             }
             if pushdata_len != 0 {
-                let immediate_val = &bytecode[i + 2..pushdata_len];
+                let immediate_val = &bytecode[i..i + pushdata_len];
+                println!("pushdata len != 0 and immediate val is {}", immediate_val);
                 self.instructions
                     .push((Opcode { 0: opcode.0 }, Some(String::from(immediate_val))));
                 i += pushdata_len;
             } else {
-                i += 2;
                 self.instructions.push((Opcode { 0: opcode.0 }, None));
             }
         }
-        println!("Instructions received: {:?}", self.instructions);
+        println!("Instructions received: {:x?}", self.instructions);
 
         &self.instructions
     }
