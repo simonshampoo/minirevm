18c7c69982e7b2b79f86f03c462fea4f9ca66c07
Simon Shamoon <simonshm5@gmail.com>
2022-12-25 21:26:26 -0600

    stacking bread

--- src/stack.rs
+++ src/stack.rs
@@ -13,12 +13,30 @@ impl Stack {
 
     pub fn push(&mut self, value: Bytes32) {
         if self.stack.len() > 1024 {
-            panic!("Stack too deep")
+            panic!("FUCKKKKKK")
         }
         self.stack.push(value);
     }
 
     pub fn pop(&mut self) {
         self.stack.pop();
+    }
+
+    pub fn dup(&mut self, x: usize) {
+        if self.stack.len() < x {
+            panic!("stack underflow")
+        }
+        if x > 16 {
+            panic!("not possible")
+        }
+
+        self.stack.swap(0, x);
+        if x == 1 {
+            self.stack.push(self.stack[0]);
+
+            self.stack.push(self.stack[x]);
+        } else {
+            todo!("need to implement algorithm for the other DUP opcodes cuz the stack output for them is weird")
+        }
     }
 }
