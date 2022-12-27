3b827ed0180d0e19fadaae2749ec821de1c5f5f8
Simon Shamoon <simonshm5@gmail.com>
2022-12-26 18:03:40 -0600

    implement DUP{x}

--- src/stack.rs
+++ src/stack.rs
@@ -1,4 +1,5 @@
 use crate::storage::Bytes32;
+use std::io::{Error, ErrorKind};
 
 pub struct Stack {
     stack: Vec<Bytes32>,
@@ -22,21 +23,20 @@ impl Stack {
         self.stack.pop();
     }
 
-    pub fn dup(&mut self, x: usize) {
-        if self.stack.len() < x {
-            panic!("stack underflow")
+    pub fn dup(&mut self, position: usize) -> Result<Bytes32, &'static str> {
+        if self.stack.len() < position {
+            return Err("fuck");
         }
-        if x > 16 {
-            panic!("not possible")
+        if position > 16 {
+            return Err("fuck");
         }
 
-        self.stack.swap(0, x);
-        if x == 1 {
-            self.stack.push(self.stack[0]);
+        let peek_index = -1 * position as i8;
 
-            self.stack.push(self.stack[x]);
-        } else {
-            todo!("need to implement algorithm for the other DUP opcodes cuz the stack output for them is weird")
-        }
+        self.stack.push(self.stack[peek_index as usize]);
+
+        self.stack.swap(0, position);
+
+        Ok(self.stack[peek_index as usize])
     }
 }
