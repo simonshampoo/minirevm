83fbbca33424165cd51c4c9d88db24c08ce38f1c
Simon Shamoon <simonshm5@gmail.com>
2022-12-27 03:43:27 -0600

    rorscach

--- src/stack.rs
+++ src/stack.rs
@@ -7,8 +7,7 @@ pub struct Stack {
 
 impl Stack {
     pub fn new() -> Self {
-        Stack {
-            stack: Vec::<Bytes32>::new(),
+        Stack { stack: Vec::<Bytes32>::new(),
         }
     }
 
@@ -30,17 +29,24 @@ impl Stack {
         self.stack.pop();
     }
 
+    fn size(&self) -> usize {
+        self.stack.len()
+    }
+
     pub fn dup(&mut self, position: usize) -> Result<Bytes32, &'static str> {
-        if self.stack.len() < position {
+
+        if self.size() > 1023 {
+            return Err("stack overflow")
+        }
+        if self.size() < position {
             return Err("fuck");
         }
         if position > 16 {
             return Err("fuck");
         }
 
-        let peek_index = -1 * position as i8;
-
-        self.stack.push(self.stack[peek_index as usize]);
+        let peek_index = self.size()-position-1;
+        self.stack.push(self.stack[peek_index]);
 
         self.stack.swap(0, position);
 
