ba26328481e587c8a7b5cce7b1032a60ca66f14c
Simon Shamoon <simonshm5@gmail.com>
2022-12-29 20:23:21 -0600

    storage time

--- src/storage.rs
+++ src/storage.rs
@@ -3,13 +3,24 @@ use std::collections::HashMap;
 pub type Bytes32 = [u8; 4];
 pub struct Storage {
     pub kvstore: HashMap<Bytes32, Bytes32>,
+    pub gas: u16,
 }
 
 impl Storage {
     pub fn new() -> Self {
         Storage {
             kvstore: HashMap::<Bytes32, Bytes32>::new(),
+            gas: 0,
         }
+    }
+
+    pub fn new_storage(&mut self, key: Bytes32, value: Bytes32) {
+
+
+    }
+
+    pub fn get_gas(&self) -> u16 {
+        self.gas
     }
 }
 
@@ -18,9 +29,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = 2+2;
+        let result = 2 + 2;
 
         assert_eq!(result, 4);
     }
-
 }
